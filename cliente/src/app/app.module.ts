60a6f6c21eae30de2220e25e5e886de75078a355
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2022-01-06 21:20:57 -0500

    Cursos iniciales

---
+++ cliente/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CrearProductoComponent,
+        ListarProductosComponent
+    ],
+    imports: [
+        BrowserModule, AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
